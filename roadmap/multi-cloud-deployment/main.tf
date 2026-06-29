24002e1c264ed4dc2c7838faff214ba25448841b
raulrodriguezmesia-blip <raulrodriguezmesia@gmail.com>
2026-06-29 15:49:39 -0500

    chore: add CI/CD and roadmap base files

---
+++ roadmap/multi-cloud-deployment/main.tf
@@ -0,0 +1,30 @@
+﻿# Terraform Multi-cloud Deployment
+
+# Provider configuration for AKS
+provider \"azurerm\" {
+  features {}
+}
+
+# Provider configuration for GKE
+provider \"google\" {
+  project = var.project_id
+  region  = var.region
+}
+
+# Main resources will be defined here
+resource \"azurerm_kubernetes_cluster\" \"main\" {
+  name                = \"-aks\"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = \"aks\"
+ 
+  default_node_pool {
+    name       = \"default\"
+    node_count = 2
+    vm_size    = \"Standard_D2_v2\"
+  }
+
+  identity {
+    type = \"SystemAssigned\"
+  }
+}
